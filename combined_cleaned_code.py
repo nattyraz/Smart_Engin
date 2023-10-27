b612b0ed5e0df0066dc3db87d854ac4cfa1cad3d
ROFOX <rgfogliami@gmail.com>
2023-10-27 15:18:57 +0200

    Add files via upload

---
+++ combined_cleaned_code.py
@@ -0,0 +1,95 @@
+
+# Combined and Cleaned-Up Code
+
+# ---- From integration_sample_code.py ----
+
+# Import Streamlit and custom NLP module
+import streamlit as st
+import traitement_langage_naturel as nlp  # Make sure to place traitement_langage_naturel.py in the same directory or adjust the import
+
+# Main function for the app
+def main():
+    # Load logo and display it
+    st.image("assets/logo.png", width=300)
+    
+    # Display friendly message and search box
+    st.write("Bonjour! Comment puis-je vous aider aujourd'hui?")
+    user_input = st.text_input("Entrez votre recherche ici...")
+    
+    # Process the user query and display results
+    if user_input:
+        # Process the query using custom NLP module
+        criteria = nlp.process_query(user_input)
+        
+        # Search in Excel based on the criteria and get results
+        search_results = nlp.search_in_excel(criteria)
+        
+        # Display the search results
+        st.write("Voici les résultats correspondant à votre recherche :")
+        st.write(search_results)
+
+# Run the app
+if __name__ == "__main__":
+    main()
+
+
+# ---- From nlp_sample_code.py ----
+
+# Import required libraries
+import openai
+import pandas as pd
+
+# Initialize OpenAI API (Replace "your_openai_api_key" with your actual API key)
+openai.api_key = "your_openai_api_key"
+
+# Function to process natural language query
+def process_query(query):
+    # Use OpenAI API to understand the query (This is a simplified example. In actual implementation, more complex logic can be used.)
+    response = openai.Completion.create(engine="text-davinci-002", prompt=query, max_tokens=50)
+    processed_query = response.choices[0].text.strip()
+    
+    # Extract search criteria (This is a placeholder. Actual implementation will require parsing the processed_query.)
+    search_criteria = {"screen_size": "15 inch", "memory": "16 GB"}
+    
+    return search_criteria
+
+# Function to search in Excel file based on the criteria
+def search_in_excel(criteria):
+    # Load Excel file
+    df = pd.read_excel("data/fichier_excel.xlsx")
+    
+    # Search logic (This is a placeholder. Actual implementation will require more complex search logic based on criteria.)
+    results = df[(df["Screen Size"] == criteria["screen_size"]) & (df["Memory"] == criteria["memory"])]
+    
+    return results
+
+# Example usage
+if __name__ == "__main__":
+    user_query = "I am looking for a 15-inch laptop with 16 GB memory."
+    criteria = process_query(user_query)
+    search_results = search_in_excel(criteria)
+    print(search_results)
+
+
+# ---- From app_sample_code.py ----
+
+# Import Streamlit
+import streamlit as st
+
+# Main function for the app
+def main():
+    # Load logo and display it
+    st.image("assets/logo.png", width=300)
+    
+    # Display friendly message and search box
+    st.write("Bonjour! Comment puis-je vous aider aujourd'hui?")
+    user_input = st.text_input("Entrez votre recherche ici...")
+    
+    # Placeholder for displaying results
+    if user_input:
+        st.write("Les résultats pour votre recherche seront affichés ici.")
+        
+# Run the app
+if __name__ == "__main__":
+    main()
+
