5e432b50aca4e7fbf7d57d784918eee72a988208
ROFOX <rgfogliami@gmail.com>
2023-10-27 18:50:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
-
-# Import Streamlit and custom NLP module
+# Import required libraries
 import streamlit as st
-import traitement_langage_naturel as nlp  # Make sure to place traitement_langage_naturel.py in the same directory or adjust the import
 import openai
 import pandas as pd
-import streamlit as st
+import os
+
+# Initialize OpenAI API (Replace "your_openai_api_key" with your actual API key)
+openai.api_key = "your_openai_api_key"
+
+# Function to process natural language query
+def process_query(query):
+    response = openai.Completion.create(engine="text-davinci-002", prompt=query, max_tokens=50)
+    processed_query = response.choices[0].text.strip()
+    return processed_query
+
+# Function to search in Excel based on the criteria
+def search_in_excel(query, excel_path):
+    df = pd.read_excel(excel_path)
+    results = df[df.apply(lambda row: row.astype(str).str.contains(query, case=False).any(), axis=1)]
+    return results
 
 # Main function for the app
 def main():
-    # Load logo and display it
     st.image("assets/header-logo.png", width=300)
-    
-    # Display friendly message and search box
     st.write("Bonjour! Comment puis-je vous aider aujourd'hui?")
     user_input = st.text_input("Entrez votre recherche ici...")
     
-    # Process the user query and display results
+    # Update this variable with the latest Excel file name
+    latest_excel_file = "path_to_latest_excel_file.xlsx"
+    
     if user_input:
-        # Process the query using custom NLP module
-        criteria = nlp.process_query(user_input)
+        processed_query = process_query(user_input)
+        search_results = search_in_excel(processed_query, latest_excel_file)
         
-        # Search in Excel based on the criteria and get results
-        search_results = nlp.search_in_excel(criteria)
-        
-        # Display the search results
         st.write("Voici les résultats correspondant à votre recherche :")
         st.write(search_results)
 
@@ -31,63 +39,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-# ---- From nlp_sample_code.py ----
-
-# Import required libraries
-
-
-# Initialize OpenAI API (Replace "your_openai_api_key" with your actual API key)
-openai.api_key = "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"
-
-# Function to process natural language query
-def process_query(query):
-    # Use OpenAI API to understand the query (This is a simplified example. In actual implementation, more complex logic can be used.)
-    response = openai.Completion.create(engine="text-davinci-002", prompt=query, max_tokens=50)
-    processed_query = response.choices[0].text.strip()
-    
-    # Extract search criteria (This is a placeholder. Actual implementation will require parsing the processed_query.)
-    search_criteria = {"screen_size": "15 inch", "memory": "16 GB"}
-    
-    return search_criteria
-
-# Function to search in Excel file based on the criteria
-def search_in_excel(criteria):
-    # Load Excel file
-    df = pd.read_excel("data/fichier_excel.xlsx")
-    
-    # Search logic (This is a placeholder. Actual implementation will require more complex search logic based on criteria.)
-    results = df[(df["Screen Size"] == criteria["screen_size"]) & (df["Memory"] == criteria["memory"])]
-    
-    return results
-
-# Example usage
-if __name__ == "__main__":
-    user_query = "I am looking for a 15-inch laptop with 16 GB memory."
-    criteria = process_query(user_query)
-    search_results = search_in_excel(criteria)
-    print(search_results)
-
-
-# ---- From app_sample_code.py ----
-
-# Import Streamlit
-
-
-# Main function for the app
-def main():
-    # Load logo and display it
-    st.image("assets/logo.png", width=300)
-    
-    # Display friendly message and search box
-    st.write("Bonjour! Comment puis-je vous aider aujourd'hui?")
-    user_input = st.text_input("Entrez votre recherche ici...")
-    
-    # Placeholder for displaying results
-    if user_input:
-        st.write("Les résultats pour votre recherche seront affichés ici.")
-        
-# Run the app
-if __name__ == "__main__":
-    main()
 
